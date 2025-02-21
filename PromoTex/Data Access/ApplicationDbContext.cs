c64904f9a8ffb53aac3a53d5335763d3b5dd8652
Pasaa2025 <kerolosmonir27@gmail.com>
2025-02-21 20:49:28 +0200

    Add project files.

---
+++ PromoTex/Data Access/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PromoTex.Models;
+
+namespace PromoTex.Data_Access
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
