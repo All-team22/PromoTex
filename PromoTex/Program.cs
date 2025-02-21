15301b66e17ec549c55fa1a05112409732b1c3cd
Pasaa2025 <kerolosmonir27@gmail.com>
2025-02-21 20:52:52 +0200

    second

--- PromoTex/Program.cs
+++ PromoTex/Program.cs
@@ -23,7 +23,7 @@ namespace PromoTex
             builder.Services.AddDbContext<ApplicationDbContext>(
                   options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
                 );
-            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(option => option.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(option => option.SignIn.RequireConfirmedAccount = false)
               .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
             builder.Services.AddScoped<IEmailSender, EmailSender>();
             var app = builder.Build();
